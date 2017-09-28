17e4a278b11ef53395ca01fa2e6025109271d13a
itajan <izaactajan@gmail.com>
2017-09-28 12:06:26 -0600

    Implemented try/catch with runtime exceptions.

--- src/edu/cnm/deepdive/algorithms/Shuffle.java
+++ src/edu/cnm/deepdive/algorithms/Shuffle.java
@@ -15,6 +15,8 @@ import java.util.Random;
  */
 public class Shuffle {
   
+  private static final String NO_INPUT_MESSAGE = "No size specified; using default value (%d).%n";
+  private static final String BAD_INPUT_MESSAGE = "Could not parse \"%s\" as an int; using default value (%d).%n";
   private static final int DEFAULT_SIZE = 50;
   /**
    * Reads a command line argument to create an array of the specified size,
@@ -25,12 +27,24 @@ public class Shuffle {
    *              (default = 50)
    */
   public static void main(String[] args) {
-    int size = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_SIZE;
+    int size = DEFAULT_SIZE;
+    try {
+      size = getSize(args);
+    } catch (NumberFormatException ex) {
+      System.out.printf(BAD_INPUT_MESSAGE, args[0], DEFAULT_SIZE);
+    } catch (ArrayIndexOutOfBoundsException ex) {
+      System.out.printf(NO_INPUT_MESSAGE,DEFAULT_SIZE);
+    } 
     int[] values = generate(size);
     System.out.println(Arrays.toString(values));
     shuffle(values);
     System.out.println(Arrays.toString(values));
   }
+
+  private static int getSize(String[] args)
+      throws ArrayIndexOutOfBoundsException, NumberFormatException {
+    return Integer.parseInt(args[0]);
+  }  
 
   private static int[] generate(int size) {
     int[] data = new int[size];
